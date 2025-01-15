ab2b5eae7d37360c7d066106f8cf69327bc9957d
Devil King <diku.dpy@gmail.com>
2025-01-15 13:22:01 +0530

    Clear

---
+++ DAL/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class Utilities
+    {
+        public static string GetConnectionString()
+        {
+            return @"data source = AVB-14\SQLEXPRESS; 
+                     initial catalog = FamilyExpenseTracker; 
+                     integrated security = sspi";
+        }
+    }
+}
